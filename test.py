4bf4d28ae665c44968b1f1bf6a37e79cbaa52833
Oscar Alemán <fretshot@gmail.com>
2019-05-14 18:27:24 -0500

    changed number output to string labels

--- test.py
+++ test.py
@@ -1,17 +1,33 @@
 import pickle
 import numpy as np
 
-# Leyendo X
+# Leyendo muestra de carros
 x = np.loadtxt("test_data.txt")
 
-# Escogiendo un numero aleatorio
+# Escogiendo un numero aleatorio de auto
 ex = np.random.randint(0,500)
 
-# Cargando la red
+# Cargando la clasificador entrenado
 loaded_model = pickle.load(open("model_trained.sav","rb"))
 
-# IDentificacion del tipo de carro
+# Identificacion del tipo de carro
 xt = x[ex,:].reshape(1,-1)
 predicted = loaded_model.predict(xt)
-print("Las caracteristicas del auto son: ",xt)
-print("Se predice que el auto es: ", predicted) 
+
+print("Las caracteristicas del auto escogido son: ",xt)
+if("1" in predicted):
+    print("Se predice que el auto es inaccesible")
+    print("Mejor optar por otra configuracion...")
+
+if("2" in predicted):
+    print("Se predice que el auto es accesible")
+    print("El auto esta pasable...")
+
+if("3" in predicted):
+    print("Se predice que el auto es bueno")
+    print("El auto no te va a defraudar...")
+
+if("4" in predicted):
+    print("Se predice que el auto es muy bueno")
+    print("Es tan bueno, que lo amarás...")
+
